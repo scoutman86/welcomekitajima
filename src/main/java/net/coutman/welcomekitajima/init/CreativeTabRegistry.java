d7b569e610561cee15784800dce8e4d5ad4d1586
the coutless <99849916+scoutman86@users.noreply.github.com>
2026-05-15 11:10:18 -0400

    become null. stupid

--- src/main/java/net/coutman/welcomekitajima/init/CreativeTabRegistry.java
+++ src/main/java/net/coutman/welcomekitajima/init/CreativeTabRegistry.java
@@ -20,14 +20,11 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.world.item.Items;
 
 public class CreativeTabRegistry {
-	public static ResourceKey<CreativeModeTab> TAB_CDS = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(WelcomeKitajima.MODID, "cds"));
 	public static ResourceKey<CreativeModeTab> TAB_SPECIALTIES = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(WelcomeKitajima.MODID, "specialties"));
 	public static ResourceKey<CreativeModeTab> TAB_BOSS_DROPS = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(WelcomeKitajima.MODID, "boss_drops"));
 	public static ResourceKey<CreativeModeTab> SMITHING_CRAFTING_ITEMS = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(WelcomeKitajima.MODID, "crafting_materials"));
 
 	public static void load() {
-		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, TAB_CDS,
-				FabricItemGroup.builder().title(Component.translatable("item_group.welcomekitajima.cds")).icon(() -> new ItemStack(ItemRegistry.LIGHT_DWELL)).build());
 		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, TAB_SPECIALTIES,
 				FabricItemGroup.builder().title(Component.translatable("item_group.welcomekitajima.specialties")).icon(() -> new ItemStack(ItemRegistry.KNUCKLE_SANDWICH)).build());
 		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, TAB_BOSS_DROPS,
